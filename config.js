223ae1b8d2b23ac7cec7f2133ee3c1482cc9b60a
Toxicsuku <152196054+Toxicsuku@users.noreply.github.com>
2023-11-27 23:17:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,81 +1,81 @@
-import { watchFile, unwatchFile } from 'fs'
-import chalk from 'chalk'
-import { fileURLToPath } from 'url'
-import fs from 'fs'
-import fetch from 'node-fetch'
-import axios from 'axios'
+import { watchFile, unwatchFile } from "fs"
+import chalk from "chalk"
+import { fileURLToPath } from "url"
+import fs from "fs"
+import fetch from "node-fetch"
+import axios from "axios"
 
 
 global.owner = [
-  ['916289408285', '♚♡𝓣𝓸𝔁𝓲𝓬_꯱𝒖𝒌𝒖♡♚', true],
-  ['918100991567', 'Bhabik♥️💀',  true],
-  [''] 
+  ["916289408285", "♚♡𝓣𝓸𝔁𝓲𝓬_꯱𝒖𝒌𝒖♡♚", true],
+  [""]
+  [""] 
 ] //Number of owners
 
-global.mods = ['916289408285','916289408285'] 
-global.prems = ['916289408285', '916289408285', '916289408285']
-global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
+global.mods = ["916289408285","916289408285"] 
+global.prems = ["916289408285", "916289408285", "916289408285"]
+global.keysZens = ["c2459db922", "37CC845916", "6fb0eff124"]
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
-global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
+global.keysxteammm = ["29d4b59a4aa687ca", "5LTV57azwaid7dXfz5fzJu", "cb15ed422c71a2fb", "5bd33b276d41d6b4", 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
 global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
-global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
+global.keysneoxrrr = ["5VC9rvNx", "cfALv5"]
 global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
-global.lolkeysapi = ['BrunoSobrino']
+global.lolkeysapi = ["BrunoSobrino"]
 
 global.APIs = { // API Prefix
-  // name: 'https://website'
-  xteam: 'https://api.xteam.xyz', 
-  dzx: 'https://api.dhamzxploit.my.id',
-  lol: 'https://api.lolhuman.xyz',
-  violetics: 'https://violetics.pw',
-  neoxr: 'https://api.neoxr.my.id',
-  zenzapis: 'https://zenzapis.xyz',
-  akuari: 'https://api.akuari.my.id',
-  akuari2: 'https://apimu.my.id',
-  nrtm: 'https://fg-nrtm.ddns.net',
-  bg: 'http://bochil.ddns.net',
-  fgmods: 'https://api-fgmods.ddns.net'
+  // name: "https://website"
+  xteam: "https://api.xteam.xyz", 
+  dzx: "https://api.dhamzxploit.my.id",
+  lol: "https://api.lolhuman.xyz",
+  violetics: "https://violetics.pw",
+  neoxr: "https://api.neoxr.my.id",
+  zenzapis: "https://zenzapis.xyz",
+  akuari: "https://api.akuari.my.id",
+  akuari2: "https://apimu.my.id",
+  nrtm: "https://fg-nrtm.ddns.net",
+  bg: "http://bochil.ddns.net",
+  fgmods: "https://api-fgmods.ddns.net"
 }
 global.APIKeys = { // APIKey Here
-  // 'https://website': 'apikey'
-  'https://api.xteam.xyz': 'd90a9e986e18778b',
-  'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
-  'https://api.neoxr.my.id': `${keysneoxr}`,	
-  'https://violetics.pw': 'beta',
-  'https://zenzapis.xyz': `${keysxxx}`, 
-  'https://api-fgmods.ddns.net': 'fg-dylux'
+  // "https://website": "apikey"
+  "https://api.xteam.xyz": "d90a9e986e18778b",
+  "https://api.lolhuman.xyz": "85faf717d0545d14074659ad",
+  "https://api.neoxr.my.id": "${keysneoxr}",	
+  "https://violetics.pw": "beta",
+  "https://zenzapis.xyz": "${keysxxx}", 
+  "https://api-fgmods.ddns.net": "fg-dylux"
 }
 
 // Sticker WM
-global.botname = '♚♡𝓣𝓸𝔁𝓲𝓬_꯱𝒖𝒌𝒖♡♚'
-global.premium = 'true'
-global.packname = '♚♡𝓣𝓸𝔁𝓲𝓬_꯱𝒖𝒌𝒖♡♚' 
-global.author = '♚♡𝓣𝓸𝔁𝓲𝓬_꯱𝒖𝒌𝒖♡♚' 
-global.igfg = '▢ Follow on Instagramhttps://www.instagram.com/invites/contact/?i=9jni427jw8g6&utm_content=6x2doke' 
-global.dygp = 'https://chat.whatsapp.com/BOLb0ICN3sAJ5dloRBw5VD'
-global.fgsc = 'https://github.com/AbhishekSuresh2/ABHISHEK-SER' 
-global.fgyt = 'https://youtube.com/@comedymelodych8468'
-global.fgpyp = 'https://youtube.com/@comedymelodych8468'
-global.fglog = 'https://replicate.delivery/pbxt/QbP6Fh3ZXwKON9SCB70ERGwwgeeSbztwKIOIzhUeXFkwnFHiA/out.png' 
+global.botname = "♚♡𝓣𝓸𝔁𝓲𝓬_꯱𝒖𝒌𝒖♡♚"
+global.premium = "true"
+global.packname = "♚♡𝓣𝓸𝔁𝓲𝓬_꯱𝒖𝒌𝒖♡♚" 
+global.author = "♚♡𝓣𝓸𝔁𝓲𝓬_꯱𝒖𝒌𝒖♡♚" 
+global.igfg = "▢ Follow on Instagramhttps://www.instagram.com/invites/contact/?i=9jni427jw8g6&utm_content=6x2doke" 
+global.dygp = "https://chat.whatsapp.com/BOLb0ICN3sAJ5dloRBw5VD"
+global.fgsc = "https://github.com/AbhishekSuresh2/ABHISHEK-SER" 
+global.fgyt = "https://youtube.com/@comedymelodych8468"
+global.fgpyp = "https://youtube.com/@comedymelodych8468"
+global.fglog = "https://replicate.delivery/pbxt/QbP6Fh3ZXwKON9SCB70ERGwwgeeSbztwKIOIzhUeXFkwnFHiA/out.png" 
 
 //imagen
-global.imagen1 = fs.readFileSync('./src/Abhi.jpg')
+global.imagen1 = fs.readFileSync("./src/Abhi.jpg")
 
 //Dont Edit Danger Zone⚠️
-global.wait = '*⌛ _Please Wait..._*'
-global.rwait = '⌛'
-global.dmoji = '🤭'
-global.done = '✅'
-global.error = '❌' 
-global.mlc = '📃'
-global.sdc = '🎶'
-global.xmoji = '🔥' 
-global.multiplier = 69 
-global.maxwarn = '3' // máxima advertencias
+global.wait = "*⌛ _Please Wait..._*"
+global.rwait = "⌛"
+global.dmoji = "🤭"
+global.done = "✅"
+global.error = "❌" 
+global.mlc = "📃"
+global.sdc = "🎶"
+global.xmoji = "🔥" 
+global.multiplier = "69" 
+global.maxwarn = "3" // máxima advertencias
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
   unwatchFile(file)
   console.log(chalk.redBright("Update 'config.js'"))
-  import(`${file}?update=${Date.now()}`)
+  import("${file}?update=${Date.now()}")
 })
